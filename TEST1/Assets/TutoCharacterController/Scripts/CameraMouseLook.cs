7c9bbb47ee740dc707cceb1284d4e42972c718c8
tarazhong <zhong.tara@gmail.com>
2017-09-24 20:48:12 +0200

    Tuto 1stPersonController
    Scripts + Assets for tutorial

---
+++ TEST1/Assets/TutoCharacterController/Scripts/CameraMouseLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class responsible for main character camera look
+/// </summary>
+public class CameraMouseLook : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothVector;
+
+    [SerializeField] // Let the value appears in Unity editor
+    private float sensitivity = 1.2f;
+    [SerializeField]
+    private float smoothing = 2.0f;
+
+    GameObject character;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Change of the mouse movement
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        
+        // Multiplying with smooth and sensitivity
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothVector.x = Mathf.Lerp(smoothVector.x, mouseDelta.x, 1f / smoothing);
+        smoothVector.y = Mathf.Lerp(smoothVector.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smoothVector;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f); // Clamp Looking up and down
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+    }
+}
---
+++ TEST1/Assets/TutoCharacterController/Scripts/MainCharacterController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Class responsible for main character controlls :
+/// Character is able to : Move(Walk and Sprint) and Jump.
+/// Show And Hide cursor with Right Click
+/// WASD to move, Maintain left shift to run
+/// To Change default inputs : Edit => Project Settings => Input
+/// </summary>
+public class MainCharacterController : MonoBehaviour
+{
+    private float walkSpeed = 5.0f;
+    private float runSpeed = 10.0f;
+    private float jumpSpeed = 5.0f;
+    private bool mouseLock;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Hide the cursor to begin with
+        Cursor.lockState = CursorLockMode.Locked;
+        mouseLock = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        ShowMouse();
+    }
+
+    // Get input and translate character (Walk and Sprint)
+    private void Move()
+    {
+        float currentSpeed = walkSpeed; 
+        // Check if the player sprints
+        if (Input.GetKey(KeyCode.LeftShift)) // GetKey returns true while the user hold down the key
+        {
+            currentSpeed = runSpeed;
+        }
+
+        float xMove = Input.GetAxis("Horizontal") * currentSpeed;
+        float zMove = Input.GetAxis("Vertical") * currentSpeed;
+        xMove *= Time.deltaTime;
+        zMove *= Time.deltaTime;
+        // Moving
+        this.transform.Translate(xMove, 0, zMove);
+    }
+
+    // Go up
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // GetKey returns true when the user press down the key
+        {
+            this.GetComponent<Rigidbody>().velocity += jumpSpeed * Vector3.up;
+        }
+    }
+
+    // Show the cursor
+    private void ShowMouse()
+    {
+        if (Input.GetMouseButtonDown(1)) // Right click down
+        {
+            mouseLock = !mouseLock;
+            if (mouseLock)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
